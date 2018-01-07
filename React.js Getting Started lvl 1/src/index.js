0321973a575c59ab1ff224a890ce699cb3d9da76
ivanttyy <ivanivanov12@gmail.com>
2018-01-07 14:23:21 +0200

    play nine stars game init

--- React.js Getting Started lvl 1/src/index.js
+++ React.js Getting Started lvl 1/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import GitCards from './GitCards';
+// import GitCards from './GitCards';
+import PlayNine from './PlayNine';
 import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<GitCards />, document.getElementById('root'));
+// ReactDOM.render(<GitCards />, document.getElementById('root'));
+ReactDOM.render(<PlayNine />, document.getElementById('root'));
 registerServiceWorker();
